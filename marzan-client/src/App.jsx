362e9d993341f6dff9ee8444330058461fe4700d
Josh-Mrzn <marzanjoshua77@gmail.com>
2026-03-28 07:52:47 +0800

    Update year and section from INF-234 to INF-233

--- marzan-client/src/App.jsx
+++ marzan-client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         Name: Joshua Marzann<br />
         Email: marzanjoshua@gm<br />
         Course: BS in Information Technology with Specialization in Mobile and Web Application<br />
-        Year and Section: 3rd Year INF-234<br />
+        Year and Section: 3rd Year INF-233<br />
         Github link: <a href="https://github.com/Josh-Mrzn/marzan-webprog" target="_blank" rel="noopener noreferrer">
         https://github.com/Josh-Mrzn/marzan-webprog
         </a>
